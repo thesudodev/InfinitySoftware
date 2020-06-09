28805f2b7177ce3c42dd50555f3bb24563b92f21
StarCrystal Team <44244999+StarCrystalTeam@users.noreply.github.com>
2020-06-09 18:56:59 -0400

    Add files via upload

---
+++ infinity.js
@@ -0,0 +1,5 @@
+//Infinity software site, coded by the StarCrystal Team.
+var member_count = 2;
+console.log("Infinity software site, coded by the StarCrystal Team.")
+console.log('%c Did someone tell you to paste or type in code here? If so, DO NOT DO IT! ', 'color: #c72a08; font-size: 24px;');
+document.getElementById("about").innerText = document.getElementById("about").innerText.replace("(Cannot read member count.)", member_count.toString())
